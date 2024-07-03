1ef13df5cd0b7e39c323e40880aca088596332f0
FacundoDiazz <facudiaz1891@gmail.com>
2024-07-02 22:08:39 -0300

    JavaS

---
+++ 04-introJavaScript/Matematic.js
@@ -0,0 +1,29 @@
+
+// Operadores Matematicos
+
+// +  -  * / % 
+
+let numUno =15;
+let numDos =50;
+
+let resultado;
+/* let resultadoSuma;
+let resultadoResta;
+let resultadoDivision;
+let resultadoMultiplicacion; */
+
+
+resultado = numUno + numDos;
+
+console.log( 15+50);
+
+console.log(numUno + numDos);
+
+console.log (resultado);
+
+
+resultado = numUno - numDos;
+
+console.log ("el resultado de la resta es: " + resultado)
+
+alert("el resultado de la resta es: " + resultado)
